3425c2410e1a136edad52c720c04ea2029d19721
Thomas Pennnant <pennantt@icloud.com>
2025-04-07 19:41:35 -0400

    Reorder URL patterns for clarity and improved routing structure

--- personal_app/urls.py
+++ personal_app/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.blog_post_list, name='blog_post_list'),  # Blog post list view
-    path('<int:post_id>/', views.blog_post_detail, name='blog_post_detail'),  # Blog post detail view
-    path('about/', views.about, name='about'),  # About page
-    path('home/', views.home, name='home'),  # Home page
+    path('', views.home, name='home'),  # Home page
+    path('blog/', views.blog_post_list, name='blog_post_list'),  # Blog list
+    path('blog/<int:post_id>/', views.blog_post_detail, name='blog_post_detail'),  # Blog detail
 ]
